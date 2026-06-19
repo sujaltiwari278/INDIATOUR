e8a5fede1cc2c339d9143be71f071ecb327d61ac
sujaltiwari278 <sujaltiwari278@gmail.com>
2026-06-19 08:10:15 +0530

    Fix leaflet markers

--- frontend/src/components/CityRouteMap.jsx
+++ frontend/src/components/CityRouteMap.jsx
@@ -10,6 +10,20 @@ import {
 } from "react-leaflet";
 
 import { cityCoordinates } from "../data/cityCoordinates";
+import L from "leaflet";
+
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
+
 
 function FitBounds({
   selectedCities,
