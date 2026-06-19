58a94e96aaf0c10ef77b93ccbd6e3afae66f1c4f
sujaltiwari278 <sujaltiwari278@gmail.com>
2026-06-19 07:09:48 +0530

    Switch to API Ninjas holidays API

--- backend/src/services/holidayService.js
+++ backend/src/services/holidayService.js
@@ -3,16 +3,20 @@ const axios = require("axios");
 const getIndianHolidays = async (year) => {
   try {
     const response = await axios.get(
-      "https://calendarific.com/api/v2/holidays",
-      {
-        params: {
-          api_key:
-            process.env.CALENDARIFIC_API_KEY,
-          country: "IN",
-          year,
-        },
-      }
-    );
+  "https://api.api-ninjas.com/v1/holidays",
+  {
+    params: {
+      country: "IN",
+      year,
+    },
+    headers: {
+      "X-Api-Key":
+        process.env.API_NINJAS_KEY,
+    },
+  }
+);
+
+return response.data;
 
     return response.data.response.holidays;
   } catch (error) {
